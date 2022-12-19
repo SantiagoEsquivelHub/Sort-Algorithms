8181198bc931f0ad1703d3679aad99d4f4df54b0
Santiago Sánchez Esquivel <santiagosancheze8.1@gmail.com>
2022-12-19 16:42:10 -0500

    Quick Sort y Selection Sort

--- Quick_Sort.js
+++ Quick_Sort.js
@@ -39,11 +39,11 @@ function quickSort(array, p, r) {
     return array;
     // Only change code above this line
 }
-
-let sortedArray = quickSort([5, 4, 3, 2, 1], 0, 4);
+let array = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]
+let sortedArray = quickSort(array, 0, array.length - 1);
 console.log(sortedArray)
 
 /* 
-Average case: 
+Average case: nlg(n)
 Worst case:  n^2
 */
